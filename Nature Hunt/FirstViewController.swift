e11965d6eb35c9589cece6a573ef915adc3caf1a
kelson <kelson.reiss@gmail.com>
2019-07-18 23:24:19 -0700

    Initial Commit

---
+++ Nature Hunt/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Nature Hunt
+//
+//  Created by Kelson Reiss on 7/18/19.
+//  Copyright © 2019 Kelson Reiss. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Nature Hunt/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Nature Hunt
+//
+//  Created by Kelson Reiss on 7/18/19.
+//  Copyright © 2019 Kelson Reiss. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
