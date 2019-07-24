4af00bda7014c4be435715d45708bfbf04f6ec9d
Adam Gleitman <adam.gleitman@gmail.com>
2019-07-23 20:58:54 -0700

    Have the home page show currently active hunts.
    Right now it shows mock data, but this can be changed once we hook things up to the database.
    
    Also do some code cleanup.

---
+++ Nature_Hunt/app/src/main/java/com/example/nature_hunt/HuntRecyclerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.nature_hunt;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HuntRecyclerAdapter extends RecyclerView.Adapter<HuntRecyclerAdapter.ViewHolder> {
+    private LayoutInflater inflater;
+    private ArrayList<HuntRecyclerItemModel> huntList;
+
+    public HuntRecyclerAdapter(Context context, ArrayList<HuntRecyclerItemModel> models){
+        inflater = LayoutInflater.from(context);
+        this.huntList = models;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.horizontal_recycler_item, parent, false );
+        HuntRecyclerAdapter.ViewHolder holder = new HuntRecyclerAdapter.ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(huntList.get(position).getImage_drawable());
+        holder.textView.setText(huntList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return huntList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+        ImageView imageView;
+
+        public ViewHolder(View v){
+            super(v);
+            textView = (TextView) v.findViewById(R.id.horizontal_text_view);
+            imageView = (ImageView) v.findViewById(R.id.horizontal_image_view);
+        }
+    }
+}
